12493ac9a3b956c266c274d5b7082c99a168957f
Scott Zuyderduyn (Scinet) <scott.zuyderduyn@utoronto.ca>
2014-09-23 16:21:32 -0400

    scinet fixes

--- test/test_matrix.cu
+++ test/test_matrix.cu
@@ -7,7 +7,7 @@ __global__ void transpose( const ecuda::matrix<float> inputMatrix, ecuda::matrix
 
 	const size_t index = threadIdx.x;
 	ecuda::matrix<float>::const_row_type row = inputMatrix[index];
-	ecuda::matrix<float>::column_type column = outputMatrix[index];
+	ecuda::matrix<float>::column_type column = outputMatrix.get_column(index);
 	ecuda::matrix<float>::column_type::iterator columnIterator = column.begin();
 	for( ecuda::matrix<float>::const_row_type::const_iterator iter = row.begin(); iter != row.end(); ++iter, ++columnIterator ) *columnIterator = *iter;
 
@@ -16,20 +16,22 @@ __global__ void transpose( const ecuda::matrix<float> inputMatrix, ecuda::matrix
 int main( int argc, char* argv[] ) {
 
 	estd::matrix<float> hostMatrix( 10, 10 );
+	float tmp = 1.0;
 	for( size_t i = 0; i < 10; ++i ) {
 		for( size_t j = 0; j < 10; ++j ) {
-			hostMatrix[i][j] = static_cast<float>((i+1)*(j+1));
+			hostMatrix[i][j] = tmp; //static_cast<float>((i+1)*(j+1));
+			tmp += 1.0;
 		}
+	}
+
+	for( size_t i = 0; i < 10; ++i ) {
+		for( size_t j = 0; j < 10; ++j ) {
+			std::cout << " " << hostMatrix[i][j];
+		}
+		std::cout << std::endl;
 	}
 
 	const ecuda::matrix<float> deviceMatrix1( hostMatrix );
-	for( size_t i = 0; i < 10; ++i ) {
-		for( size_t j = 0; j < 10; ++j ) {
-			std::cerr << " " << hostMatrix[i][j];
-		}
-		std::cerr << std::endl;
-	}
-
 	ecuda::matrix<float> deviceMatrix2( 10, 10 );
 
 	dim3 dimBlock( 10, 1 ), dimGrid( 1, 1 );
