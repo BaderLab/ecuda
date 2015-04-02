c4e9b156f6fa657c1f32921b78b7e38a329d0378
Scott Zuyderduyn (Scinet) <scott.zuyderduyn@utoronto.ca>
2015-04-02 16:53:33 -0400

    scinet changes

--- include/ecuda/views.hpp
+++ include/ecuda/views.hpp
@@ -128,6 +128,7 @@ private:
 		if( n < 0 or static_cast<size_type>(n) != length ) return; // nothing happens
 		for( pointer p = ptr; first != last; ++p, ++first ) *p = *first;
 		#else
+std::cerr << "assign() n=" << n << " vs length=" << length << std::endl;
 		if( n < 0 or static_cast<size_type>(n) != length ) throw std::length_error( EXCEPTION_MSG("__device_sequence::assign first,last does not span the correct number of elements") );
 		CUDA_CALL( cudaMemcpy<typename std::remove_const<value_type>::type>( ptr, first.operator->(), length, cudaMemcpyDeviceToDevice ) );
 		#endif
@@ -213,9 +214,19 @@ public:
 	HOST DEVICE inline size_type size() const __NOEXCEPT__ { return length; }
 
 	HOST DEVICE inline iterator begin() __NOEXCEPT__ { return iterator(ptr); }
-	HOST DEVICE inline iterator end() __NOEXCEPT__  { return iterator(ptr+static_cast<int>(length)); }
+	HOST DEVICE inline iterator end() __NOEXCEPT__  {
+		typename iterator::pointer p = ptr;
+		p += static_cast<int>(length);
+		return iterator(p);
+	}
+//return iterator(ptr+static_cast<int>(length)); }
 	HOST DEVICE inline const_iterator begin() const __NOEXCEPT__ { return const_iterator(ptr); }
-	HOST DEVICE inline const_iterator end() const __NOEXCEPT__ { return const_iterator(ptr+static_cast<int>(length)); }
+	HOST DEVICE inline const_iterator end() const __NOEXCEPT__ {
+		typename iterator::pointer p = ptr;
+		p += static_cast<int>(length);
+		return const_iterator(p);
+	}
+//return const_iterator(ptr+static_cast<int>(length)); }
 
 	HOST DEVICE inline reverse_iterator rbegin() __NOEXCEPT__ { return reverse_iterator(end()); }
 	HOST DEVICE inline reverse_iterator rend() __NOEXCEPT__ { return reverse_iterator(begin()); }
@@ -389,12 +400,20 @@ private:
 
 	template<typename T2,typename PointerType2,typename RowCategory2,typename ColumnCategory2>
 	HOST void copy_to( __device_grid<T2,PointerType2,RowCategory2,ColumnCategory2>& grid, contiguous_sequence_tag, contiguous_device_iterator_tag, device_iterator_tag ) const {
+std::cerr << "performing device grid to device grid transfer" << std::endl;
 		if( grid.number_rows() != number_rows() or grid.number_columns() != number_columns() ) throw std::length_error( EXCEPTION_MSG("__device_grid::operator>> target __device_grid does not match the size of source __target_grid") );
+std::cerr << "cp1" << std::endl;
 		for( size_type i = 0; i < number_rows(); ++i ) {
+std::cerr << "PRCESSING row " << i << std::endl;
 			typename __device_grid<T2,PointerType2,RowCategory2,ColumnCategory2>::row_type dest = grid.get_row(i);
+std::cerr << "dest.size()=" << dest.size() << std::endl;
 			const_row_type src = get_row(i);
+std::cerr << "src.size()=" << src.size() << std::endl;
+std::cerr << "iterdiff=" << ( src.end()-src.begin() ) << std::endl;
 			dest.assign( src.begin(), src.end() );
+std::cerr << "FINISHED row " << i << std::endl;
 		}
+std::cerr << "cp2" << std::endl;
 	}
 
 public:
