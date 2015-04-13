5b37e2e5d6648212e012820df02675f58a1e2181
Scott Zuyderduyn <scottzed@hotmail.com>
2015-04-13 12:10:50 -0400

    finalizing for release

--- compile.bash
+++
@@ -1,2 +0,0 @@
-/usr/local/cuda/bin/nvcc -arch=sm_21 -Xcompiler -fopenmp -c test/test_array.cu -o obj/test/test_array.cu.o
-g++ -O3 -flto -march=native -fopenmp -L/usr/local/cuda/lib64 obj/test/test_array.cu.o -lcudart -o bin/test/test_array
--- include/ecuda/config.hpp
+++ include/ecuda/config.hpp
@@ -1,14 +1,7 @@
-///
-/// If the estd library is installed, allow
-/// its use within ecuda.  This allows the
-/// estd::matrix and estd::cube classes to
-/// be used with ecuda::matrix and ecuda::cube.
-///
-#define HAVE_ESTD_LIBRARY 0
-
-///
-/// If the GNU Scientific Library (GSL) is
-/// installed, allow its use within ecuda.
-/// This allows the estd::matrix and estd::cube
-/// classes to utilize gsl_matrix.
-#define HAVE_GNU_SCIENTIFIC_LIBRARY 0
+/**
+ * This file used to have preprocessor guards to include
+ * certain external libraries, but these conditional
+ * dependencies have all been removed.
+ *
+ * This file is here for future use.
+ */
